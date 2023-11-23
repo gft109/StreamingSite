f5985f1023a47f037fddd03c6a659037c4f550e3
Gustavo Ferreira <68303698+gft109@users.noreply.github.com>
2023-11-23 11:40:38 -0300

    front end + js
    front end + funcionalidades com js (músicas e imagens ainda no proprio projeto - sem banco de dados)

---
+++ music/src/main/java/com/north/music/model/Music.java
@@ -0,0 +1,34 @@
+
+package com.north.music.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Music")
+public class Music {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+    private String name;
+    
+    private String artist;
+    
+    @Lob
+    private byte[] image;
+    
+    @Lob
+    private byte[] audio;
+}
